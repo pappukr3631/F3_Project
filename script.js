d4eca23c7d5744b526707c0f888ace366200f3a7
pappukr3631 <100488442+pappukr3631@users.noreply.github.com>
2023-08-04 21:33:00 +0530

    landing page

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 // myProducts.filter((item)=>item.title.includes(search.value))
 
 // myCartProductArray = myProducts.filter((item)=> myCartIDs.includes(item.id))
+document.getElementById('navigateTOCart').addEventListener('click',(e)=>{
+    const showname = JSON.parse(sessionStorage.getItem('loginUser'));
+
+    if(showname != null){
+       window.location.href = "./cart";
+    }else{
+      alert("Please Login");
+      window.location.href = "./login";
+    }
+
+})
+
+
+document.getElementById('navigateToProfile').addEventListener('click',(e)=>{
+    const showname = JSON.parse(sessionStorage.getItem('loginUser'));
+
+    if(showname != null){
+       window.location.href = "./profile";
+    }else{
+      alert("Please Login");
+      window.location.href = "./login";
+    }
+})
